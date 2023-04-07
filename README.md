433240961a2f3327409b21ccd5161514472d5f50
ccf2012 <crlf66@163.com>
2023-04-07 23:59:19 +0800

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# tvshow2plexmatch
+read tvshow.nfo and generate .plexmatch file
+
+
+## 使用
+```
+python3 tvshow2plexmatch.py -h
+usage: tvshow2plexmatch.py [-h] dir
+
+read tvshow.nfo and generate .plexmatch file.
+
+positional arguments:
+  dir         folder path.
+
+options:
+  -h, --help  show this help message and exit
+```
+
+## 例子
+```
+python3 tvshow2plexmatch.py  /gd/tv/other
+```
+
---
+++ tvshow2plexmatch.py
@@ -0,0 +1,75 @@
+import os
+import glob
+import re
+import argparse
+import xml.etree.ElementTree as ET
+
+
+def uselessFile(entryName):
+    return entryName in ['@eaDir', '.DS_Store', '.@__thumb']
+
+def tryint(instr):
+    try:
+        string_int = int(instr)
+    except ValueError:    
+        string_int = 0
+    return string_int
+
+
+def mkPlexMatch(targetDir, tmdbid, title, year):
+    pmfilepath = os.path.join(targetDir, '.plexmatch')
+    with open(pmfilepath, "w") as pmfile:
+        pmfile.write("Title: %s\ntmdbid: %s\n" %
+                    (title, tmdbid))
+        intyear = tryint(year)
+        if intyear > 1990:
+            pmfile.write("Year: %s\n" % (year))
+
+
+def rename_dirs_with_tmdb(root_dir):
+    for index, dirname in enumerate(os.listdir(root_dir)):
+        if uselessFile(dirname):
+            continue
+
+        dirpath = os.path.join(root_dir, dirname)
+        if os.path.isdir(dirpath):
+            nfo_found = False
+            for subfile in os.listdir(dirpath):
+                if os.path.isdir(os.path.join(dirpath, subfile)):
+                    # print(f"{subfile} skip")
+                    continue
+
+                if subfile.endswith('.nfo'):
+                    nfo_path = os.path.join(dirpath, subfile)
+
+                    tree = ET.parse(nfo_path)
+                    root = tree.getroot()
+
+                    tmdb_elem = root.find('tmdbid')
+                    year_elem = root.find('year')
+                    title_elem = root.find('title')
+                    if tmdb_elem is not None:
+                        nfo_found = True
+                        print(f"{index} : {tmdb_elem.text}, {title_elem.text} ({year_elem.text})")
+                        mkPlexMatch(dirpath, tmdb_elem.text, title_elem.text, year_elem.text)
+                        break
+            if not nfo_found:
+                thedirname = os.path.basename(dirpath)
+                print(f"{index} : {thedirname} tmdbid not found")
+
+
+def loadArgs():
+    global ARGS
+    parser = argparse.ArgumentParser(description='read tvshow.nfo and generate .plexmatch file.')
+    parser.add_argument('dir', help='folder path.')
+    ARGS = parser.parse_args()
+    ARGS.dir = os.path.expanduser(ARGS.dir)
+
+
+def main():
+    loadArgs()
+    rename_dirs_with_tmdb(ARGS.dir)
+
+
+if __name__ == '__main__':
+    main()
